724d62656967c05fcb28b11dba24c1919f008d9e
internetStranger <142758257+internetStranger@users.noreply.github.com>
2024-10-23 20:09:12 -0400

    Main menu & bird obstacle
    Created a main menu with a quit and play button
    *play button leads to stage select menu
    *need to add a second level
    
    Created the bird obstacle and background
    *bird obstacle is created when background bird reached edge of screen

---
+++ Assets/BirdScript.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdScript : MonoBehaviour
+{
+    private Vector3 pos;
+    public float move_speed;
+    public float offset;
+    [SerializeField] GameObject bird;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pos = Camera.main.WorldToViewportPoint(transform.position);
+
+        move();
+
+        checkPos();
+    }
+
+    private void move()
+    {
+        if(this.tag == "obstacle")
+            transform.position += new Vector3(move_speed, 0, 0);
+        else
+            transform.position += new Vector3(-1*move_speed,0,0);
+    }
+
+    private void checkPos()
+    {
+        if(this.tag == "obstacle")
+        {
+            //obstacle bird reaches right side of screen
+            if(pos.x > 1.1)
+                Destroy(this.gameObject);
+        }
+        else
+        {
+            //background bird reaches left side of screen
+            if(pos.x < -0.1)
+            {
+                Instantiate(bird, new Vector3(transform.position.x - offset, transform.position.y, 0), Quaternion.identity);
+                Destroy(this.gameObject);
+                
+            }
+        }
+    }
+}
